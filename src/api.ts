f385a8ab6b643ed55028eb9b3a5836682333ae6b
Daniel Avelino <daniel_avelino@outlook.com>
2021-01-11 20:25:58 -0300

    mapbox

--- src/api.ts
+++ src/api.ts
@@ -2,6 +2,13 @@ import axios from "axios";
 
 const url = "http://localhost:8080";
 
+const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
+
 export function fetchProducts() {
     return axios(url + "/products")
 }
+
+export function fetchLocalMapBox(local: string) {
+    return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`);
+}
+
